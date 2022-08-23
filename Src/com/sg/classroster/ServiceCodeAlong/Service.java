ad16f51c6742e434c888a1caa57a632939d4f57d
gary-programing <garybrolly@gmail.com>
2022-08-23 16:05:39 +0100

    01: start again

---
+++ Src/com/sg/classroster/ServiceCodeAlong/Service.java
@@ -0,0 +1,17 @@
+package com.sg.classroster.ServiceCodeAlong;
+
+public interface Service {
+    void createStudent(Student student) throws
+            ClassRosterDuplicateIdException,
+            ClassRosterDataValidationException,
+            ClassRosterPersistenceException;
+
+    List<Student> getAllStudents() throws
+            ClassRosterPersistenceException;
+
+    Student getStudent(String studentId) throws
+            ClassRosterPersistenceException;
+
+    Student removeStudent(String studentId) throws
+            ClassRosterPersistenceException;
+}
